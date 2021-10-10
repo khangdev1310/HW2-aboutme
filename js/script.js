b5b55b808d30e9fd7b6d1ec9cbd06c1905a61ac9
Khang <khangvippro357@gmail.com>
2021-10-10 12:07:00 +0700

    UI home page default

--- js/script.js
+++ js/script.js
@@ -4,3 +4,12 @@ const navDiv = document.querySelector('.navbar-collapse');
 navBtn.addEventListener('click', () => {
     navDiv.classList.toggle('showNav')
 } )
+
+let resizeTimer;
+window.addEventListener('resize', () => {
+    document.body.classList.add('resize-animation-stopper');
+    clearTimeout(resizeTimer)
+    resizeTimer = setTimeout(() => {
+        document.body.classList.remove('resize-animation-stopper')
+    },400)
+})
